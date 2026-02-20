fc67d3786a9116750234c928405df9bd8f6b231f
aakashdixit22 <aakashdixit227@gmail.com>
2026-02-20 14:30:00 +0530

    Add MongoDB database connection configuration

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Database connection failed", error);
+    process.exit(1);
+  }
+};
+
+export default dbConnection;
